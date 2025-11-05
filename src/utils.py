6768df9c6316d2842ff8fb2c39c36990e39b1730
serpette.e2102229@etud.univ-ubs.fr <srpgwen@gmail.com>
2025-11-05 15:50:51 +0100

    changement minim

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,14 @@ import cv2
 import os
 import numpy as np
 
-#====================================================================================================
+#===============================================================
 # méthode getTemplates
 # input :
 #   - path : STRING = chemin vers les images
 #   - extensions : STRING = extension des images
 # output :
 #    - templates : DICT = dictionnaire des images avec leur noms
-#====================================================================================================
+#===============================================================
 def getTemplates(path) :
     templates = {}
     for file in os.listdir(path):
@@ -18,16 +18,16 @@ def getTemplates(path) :
         templates[filename] = template
     return templates
 
-#====================================================================================================
+#=======================================================================
 # méthode getFaceCard
 # input :
-#   - pts : ARRAY_LIKE = image de face
-#   - pts : ??? = ???
+#   - image_ : ARRAY_LIKE = image entière composé de la zone à redresser
+#   - approx_ : ??? = ???
 # output :
 #   - warp : ARRAY_LIKE = image vu de face
-#====================================================================================================
-def getFaceCard(image, approx):
-    pts = np.array([p[0] for p in approx], dtype=np.float32)
+#=======================================================================
+def getFaceCard(image_, approx_):
+    pts = np.array([p[0] for p in approx_], dtype=np.float32)
     src = np.zeros((4, 2), dtype="float32")
     s = pts.sum(axis=1)
     src[0] = pts[np.argmin(s)]
@@ -44,24 +44,28 @@ def getFaceCard(image, approx):
         [0, height - 1]
     ], dtype="float32")
     H, _ = cv2.findHomography(src, dst)
-    warp = cv2.warpPerspective(image, H, (width, height))
+    warp = cv2.warpPerspective(image_, H, (width, height))
     return warp
 
-
-
-# rank_name, rank_score = compare(rank, template_ranks, roi_size_x, roi_size_y)
-def compare(image, templates, size_x, size_y) :
+#==============================================================================================
+# méthode compare
+# input :
+#   - image_ : ARRAY_LIKE = image à comparer
+#   - templates_ : LIST_LIKE[ ARRAY_LIKE ] = liste des images de références
+#   - size_x_ : INT = taille pour le redimensionnement sur x
+#   - size_y_ : INT = taille pour le redimensionnement sur y
+# output :
+#   - best_name : STRING = nom de l'image de référence la plus ressemblante
+#   - best_score : INT = nombre de pixel différent entre image_ et l'image la plus ressemblante
+#==============================================================================================
+def compare(image_, templates_, size_x_, size_y_) :
     best_name = "?"
-    best_score = size_x*size_y
-    scanned_image = cv2.resize(image, (size_x, size_y))
-    for template_name, template in templates.items() :
-        template = cv2.resize(template, (size_x, size_y))
+    best_score = size_x_*size_y_
+    scanned_image = cv2.resize(image_, (size_x_, size_y_))
+    for template_name, template in templates_.items() :
+        template = cv2.resize(template, (size_x_, size_y_))
         _, template = cv2.threshold(template, 175, 255, cv2.THRESH_BINARY_INV)
         comparaison = cv2.bitwise_xor(scanned_image, template)
-        # cv2.imshow("scanned_image", scanned_image)
-        # cv2.imshow("template", template)
-        # cv2.imshow("comparaison", comparaison)
-        # cv2.waitKey(0)
         score = cv2.countNonZero(comparaison)
         if score < best_score :
             best_score = score
